b8df9854f6751e60fef0fc384fd48d358ba07034
Andrey Shmelev <shmelev@korvas.org>
2017-12-04 09:06:52 +0500

    добиваем 415 виджет

--- Widgets/MoveOrderWidget415/moveorder415.cpp
+++ Widgets/MoveOrderWidget415/moveorder415.cpp
@@ -12,3 +12,58 @@ MoveOrder415::~MoveOrder415()
 {
     delete ui;
 }
+
+bool MoveOrder415::getRegistration() const
+{
+    return registration;
+}
+
+void MoveOrder415::setRegistration(bool value)
+{
+    registration = value;
+}
+
+void MoveOrder415::updateWidgetGui(QString gtinstring, QString batchstring, QString SNstring, QString tnvedstring, QString expstring)
+{
+
+}
+
+bool MoveOrder415::CheckMedicamentinDB(medicament *m)
+{
+
+}
+
+void MoveOrder415::ToggleRegistration()
+{
+
+}
+
+void MoveOrder415::StartRegistrationProcess()
+{
+
+}
+
+void MoveOrder415::StopRegistrationProcess()
+{
+
+}
+
+void MoveOrder415::GetMedicament(medicament *m)
+{
+
+}
+
+void MoveOrder415::updateGUI()
+{
+
+}
+
+void MoveOrder415::AddMedicamentToTable(medicament *m)
+{
+
+}
+
+void MoveOrder415::AddMedicamentToDB(medicament *m)
+{
+
+}
--- Widgets/MoveOrderWidget415/moveorder415.h
+++ Widgets/MoveOrderWidget415/moveorder415.h
@@ -2,6 +2,8 @@
 #define MOVEORDER415_H
 
 #include <QWidget>
+#include <medicament.h>
+#include <manufacturer.h>
 
 namespace Ui {
 class MoveOrder415;
@@ -15,8 +17,41 @@ public:
     explicit MoveOrder415(QWidget *parent = 0);
     ~MoveOrder415();
 
+    bool getRegistration() const;
+    void setRegistration(bool value);
+
 private:
     Ui::MoveOrder415 *ui;
+
+
+private:
+
+    void updateWidgetGui(QString gtinstring, QString batchstring, QString SNstring, QString tnvedstring, QString expstring);
+    bool registration ;
+    medicament * ScannedMedicament;
+    QList<medicament *> MedicamentsList;
+    bool CheckMedicamentinDB(medicament * m);
+
+public slots:
+
+    void ToggleRegistration();
+    void StartRegistrationProcess();
+    void StopRegistrationProcess();
+
+private slots:
+
+    void GetMedicament (medicament * m);
+    void updateGUI();
+
+    void AddMedicamentToTable(medicament * m);
+    void AddMedicamentToDB(medicament * m);
+
+signals:
+
+    void RegistrationStarted();
+    void StopRegisterControlSamples();
+    void RegistrationToggled();
+    void RegistrationCompleted(QList<medicament *> MedList , uint8_t controlsamplestype);
 };
 
 #endif // MOVEORDER415_H
